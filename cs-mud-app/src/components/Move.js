5e954e3df282ebcd9c5bfe02ff18a9be1637ffa5
Michael <mdlevick@gmail.com>
2020-04-27 17:37:16 -0500

    did buttons, starting on logic

---
+++ cs-mud-app/src/components/Move.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+import React, { useState } from "react";
+
+
+const Move = (props) => {
+
+    axios
+
+return(
+    <div>
+        <button onClick={}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
+        <button onClick={}>\u21D0&#8656;&lArr;%u21D0%E2%87%90</button>
+        <button onClick={}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
+        <button onClick={}>\u21D3&#8659;&dArr;%u21D3%E2%87%93</button>
+    </div>
+)
+};
+
+export default Move;
