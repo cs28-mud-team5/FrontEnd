6d516ca109d242b5e6e1421b926175e7565844c5
Michael <mdlevick@gmail.com>
2020-04-28 13:17:33 -0500

    started work on move

--- cs-mud-app/src/components/Move.js
+++ cs-mud-app/src/components/Move.js
@@ -3,16 +3,31 @@ import React, { useState } from "react";
 
 
 const Move = (props) => {
+    const [save, setSave] = useState([])
+    const [direction, setDirection] = useState("");
+    const [display, setDisplay]
+   
 
-    axios
+const handleMove = (e) => {
+  e.preventDefault(); 
+  setDirection(e.target.name);
+  setSave(  );
+  axiosWithAuth()
+    .post("/adv/move/", direction, localStorage.getItem("token"))
+    .then((res) => {
+      
+    })
+    .catch((err) => console.log(""));
+};
 
 return(
     <div>
-        <button onClick={}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
-        <button onClick={}>\u21D0&#8656;&lArr;%u21D0%E2%87%90</button>
-        <button onClick={}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
-        <button onClick={}>\u21D3&#8659;&dArr;%u21D3%E2%87%93</button>
+        <button name="n" onClick={handleMove}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
+        <button name="w" onClick={handleMove}>\u21D0&#8656;&lArr;%u21D0%E2%87%90</button>
+        <button name="e" onClick={handleMove}>\u21D2&#8658;&rArr;%u21D2%E2%87%92</button>
+        <button name="s" onClick={handleMove}>\u21D3&#8659;&dArr;%u21D3%E2%87%93</button>
     </div>
+    
 )
 };
 
