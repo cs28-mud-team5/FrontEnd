16be3af856e8c23916a57c8618057a013beea32c
Indigo Richards <thedomesticdingo@gmail.com>
2020-04-29 10:28:55 -0700

    Movement functional
    Changed movement mechanic from arrow buttons to user form input. Getting 200 from server and moving as intended

--- cs-mud-app/src/components/Move.js
+++ cs-mud-app/src/components/Move.js
@@ -1,36 +1,42 @@
 import React, { useState, useContext } from "react";
 import { DungeonContext } from "../contexts/DungeonContext";
 import axiosWithAuth from "./axiosWithAuth";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 const Move = (props) => {
   const { player, setPlayer } = useContext(DungeonContext);
   const [direction, setDirection] = useState({ direction: "" });
 
+  const handleChanges = (e) => {
+    setDirection({ ...direction, [e.target.name]: e.target.value });
+  };
+
   const handleMove = (e) => {
     e.preventDefault();
-    setDirection({ ...direction, direction: e.target.value });
+    console.log(direction);
     axiosWithAuth()
       .post("/adv/move/", direction)
       .then((res) => {
-        setPlayer(res);
+        console.log(res);
+        setPlayer(res.data);
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <div>
-      <button name="n" value={direction} onClick={handleMove}>
-        &#8657;
-      </button>
-      <button name="w" value={direction} onClick={handleMove}>
-        &#8656;
-      </button>
-      <button name="e" value={direction} onClick={handleMove}>
-        &#8658;
-      </button>
-      <button name="s" value={direction} onClick={handleMove}>
-        &#8659;
-      </button>
+      <Form onSubmit={handleMove}>
+        <FormGroup>
+          <Label for="direction">Direction: (n, s, e, or w)</Label>
+          <Input
+            type="text"
+            name="direction"
+            onChange={handleChanges}
+            value={direction.direction}
+          />
+        </FormGroup>
+        <Button>Move</Button>
+      </Form>
     </div>
   );
 };
